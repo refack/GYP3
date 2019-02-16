d8a07a862f6a973dcc8fd20ef328a7d423967ae6
Refael Ackermann <refack@gmail.com>
2019-02-15 19:11:36 -0500

    src: refactoring & yack shaving for Python 3

--- pylib/gyp/MSVSNew.py
+++ pylib/gyp/MSVSNew.py
@@ -57,7 +57,8 @@ def MakeGuid(name, seed='msvs_new'):
   not change when the project for a target is rebuilt.
   """
   # Calculate a MD5 signature for the seed and name.
-  d = _new_md5(str(seed) + str(name)).hexdigest().upper()
+  key = (str(seed) + str(name)).encode('utf-8')
+  d = _new_md5(key).hexdigest().upper()
   # Convert most of the signature to GUID form (discard the rest)
   guid = ('{' + d[:8] + '-' + d[8:12] + '-' + d[12:16] + '-' + d[16:20]
           + '-' + d[20:32] + '}')
