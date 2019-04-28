joaocgreis/GYP

mac, libtest: cleanup & dedup detection code

test/ios/gyptest-app-ios.py
@@ -9,7 +9,7 @@ import XCodeDetect
 
 test = TestGyp.TestGyp(formats=['xcode', 'ninja'], platforms=['darwin'])
 
-if not XCodeDetect.XCodeDetect.HasIPhoneSDK():
+if not XCodeDetect.HasIPhoneSDK():
   test.skip_test('IPhone SDK not installed')
 
 

test/ios/gyptest-deployment-target.py
@@ -13,7 +13,7 @@ import XCodeDetect
 
 test = TestGyp.TestGyp(formats=['make', 'ninja', 'xcode'], platforms=['darwin'])
 
-if not XCodeDetect.XCodeDetect.HasIPhoneSDK():
+if not XCodeDetect.HasIPhoneSDK():
   test.skip_test('IPhone SDK not installed')
 
 test.run_gyp('deployment-target.gyp', chdir='deployment-target')

refack/GYP3

test/ios/gyptest-app-ios.py
@@ -9,7 +9,7 @@ import XCodeDetect
 
 test = TestGyp.TestGyp(formats=['xcode', 'ninja'], platforms=['darwin'])
 
-if not XCodeDetect.XCodeDetect.HasIPhoneSDK():
+if not XCodeDetect.IPhoneSDKPath():
   test.skip_test('IPhone SDK not installed')
 
 

test/ios/gyptest-deployment-target.py
@@ -13,7 +13,7 @@ import XCodeDetect
 
 test = TestGyp.TestGyp(formats=['make', 'ninja', 'xcode'], platforms=['darwin'])
 
-if not XCodeDetect.XCodeDetect.HasIPhoneSDK():
+if not XCodeDetect.IPhoneSDKPath():
   test.skip_test('IPhone SDK not installed')
 
 test.run_gyp('deployment-target.gyp', chdir='deployment-target')