cd60d8509164f7cb77cba71a05a253d2ee2090a7
Christian Clauss <cclauss@me.com>
2019-07-24 12:08:59 -0400

    src: use ==/!= to compare str, bytes, and int literals
    Add a more flake8 tests
    
    * F63 tests are usually about the confusion between identity and equality in Python.
    * F7 tests logic errors and syntax errors in type hints

--- test/linux/ldflags-duplicates/check-ldflags.py
+++ test/linux/ldflags-duplicates/check-ldflags.py
@@ -13,16 +13,16 @@ from __future__ import print_function
 import sys
 
 def CheckContainsFlags(args, substring):
-  if args.find(substring) is -1:
+  found = substring in args
+  if not found:
     print('ERROR: Linker arguments "%s" are missing in "%s"' % (substring,
                                                                 args))
-    return False;
-  return True;
+  return found
 
 if __name__ == '__main__':
   args = " ".join(sys.argv)
-  print("args = " +args)
-  if not CheckContainsFlags(args, 'lib1.a -Wl,--no-whole-archive') \
-    or not CheckContainsFlags(args, 'lib2.a -Wl,--no-whole-archive'):
-    sys.exit(1);
+  print("args = " + args)
+  if (not CheckContainsFlags(args, 'lib1.a -Wl,--no-whole-archive')
+      or not CheckContainsFlags(args, 'lib2.a -Wl,--no-whole-archive')):
+    sys.exit(1)
   sys.exit(0)
